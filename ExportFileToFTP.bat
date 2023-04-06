f4d2d955ae2aca2f357e72a36ad5b49c918e9c50
Jechisto <130042095+Jechisto@users.noreply.github.com>
2023-04-06 10:25:58 +0200

    Add files via upload

---
+++ ExportFileToFTP.bat
@@ -0,0 +1,7 @@
+open 10.10.10.10
+Ftp_name
+PASSWORD
+cd PATH
+mput C:\Export\file.csv
+mput C:\Export\file.csv
+bye
---
+++ start app.bat
@@ -0,0 +1,8 @@
+cd C:\Users\Aries_Jech\
+cd Desktop\
+cd Python MariaDB\
+cd Test
+ChangeName.py
+
+cd C:\Users\Aries_Jech\Desktop\Python MariaDB\Test\
+ChangeName.py
